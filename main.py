282fc3a6fa5ee581b4565954dc22e292b44dc7fd
Kyle Nahas <kylenahas@gmail.com>
2022-05-16 12:03:17 +0000

    Websocket connection to multiple connections working

--- main.py
+++ main.py
@@ -32,17 +32,30 @@ import asyncio
 import datetime
 import random
 import websockets
+import netifaces
 
-dmx_frame_queue = Queue(maxsize=100)
+# dmx_frame_queue = Queue(maxsize=100)
+dmx_frame_queue = asyncio.Queue(maxsize=10000)
+ws_active_connections = set()
+sys_ip_address = netifaces.ifaddresses('enp0s3')[netifaces.AF_INET][0].get('addr')
 
-async def time(websocket, path):
+
+async def serve_data_to_all_clients():
     while True:
-        # now = datetime.datetime.utcnow().isoformat() + 'Z'
-        # await websocket.send(now)
-        color = 'rgb(' + str(random.randint(0, 255)) + ', ' + str(random.randint(0, 255)) + ', '\
-                + str(random.randint(0, 255)) + ')'
-        await websocket.send(color)
-        await asyncio.sleep(random.random() * 3)
+        print("Server")
+        frame = await dmx_frame_queue.get()
+        for websocket in ws_active_connections:
+            print("Sending to: " + str(websocket.id))
+
+            print("Data: " + str(frame[0:3]))
+            offset = 0
+            color = 'rgb(' + str(frame[0 + offset]) + ', ' + str(frame[1 + offset]) + ', ' + str(
+                frame[2 + offset]) + ')'
+            await websocket.send(color)
+
+        # await asyncio.sleep(0.1)
+
+
 
 
 async def serve_latest_color(websocket, path):
@@ -55,11 +68,53 @@ async def serve_latest_color(websocket, path):
             await websocket.send(color)
 
 
-start_server = websockets.serve(serve_latest_color, '192.168.56.103', 5678)
+async def start_server():
+    print("WebSockets Server Started:")
+    print(" -> Using IP Address: " + sys_ip_address)
+    async with websockets.serve(ws_handler, sys_ip_address, 5678):
+        await asyncio.Future()  # run forever
+
+async def ws_handler(websocket):
+    print("New ws client: " + str(websocket.id))
+    ws_active_connections.add(websocket)
+    try:
+        await websocket.wait_closed()
+    finally:
+        print("Removed ws client: " + str(websocket.id))
+        ws_active_connections.remove(websocket)
+
+
+async def send(websocket, message):
+    try:
+        await websocket.send(message)
+    except websockets.ConnectionClosed:
+        pass
+
+async def ws_main_loop():
+    await asyncio.gather(
+        start_server(),
+        serve_data_to_all_clients()
+    )
+    # loop = asyncio.new_event_loop()
+    # loop.run_until_complete(start_server())
+    # loop.run_forever()
+    # asyncio.get_event_loop().run_until_complete(start_server())
+    # asyncio.get_event_loop().run_forever()
+    # asyncio.set_event_loop(start_server())
+    # asyncio.get_event_loop().run_forever()
 
 
 def NewData(data):
-    dmx_frame_queue.put(data)
+    try:
+        dmx_frame_queue.put_nowait(data)
+    except asyncio.queues.QueueFull:
+        print("DMX Queue full")
+    # frame = data
+    # print("Data: " + str(frame[0:3]))
+    # offset = 0
+    # color = 'rgb(' + str(frame[0 + offset]) + ', ' + str(frame[1 + offset]) + ', ' + str(frame[2 + offset]) + ')'
+    # for websocket in ws_active_connections.copy():
+    #     asyncio.create_task(send(websocket, color))
     # print(data[0:3])
 
 
@@ -72,13 +127,11 @@ def Usage():
   -h, --help                Display this help message and exit.
   -u, --universe <universe> Universe number."""))
 
-
-def ws_main_loop():
-    # asyncio.get_event_loop().run_until_complete(start_server)
-    # asyncio.get_event_loop().run_forever()
-    asyncio.set_event_loop(start_server)
-    asyncio.get_event_loop().run_forever()
-
+async def to_thread(func, /, *args, **kwargs):
+    loop = asyncio.get_running_loop()
+    ctx = contextvars.copy_context()
+    func_call = functools.partial(ctx.run, func, *args, **kwargs)
+    return await loop.run_in_executor(None, func_call)
 
 def ola_main_loop():
   try:
@@ -103,14 +156,21 @@ def ola_main_loop():
 
 
 if __name__ == "__main__":
-    # ws_thread = threading.Thread(target=ws_main_loop)
+    print("OLA Server Started:")
+    ws_thread = threading.Thread(target=ws_main_loop)
     ola_thread = threading.Thread(target=ola_main_loop)
 
     # ws_thread.start()
     ola_thread.start()
 
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+    # to_thread(ola_main_loop())
+    # to_thread(start_server())
+    # ws_main_loop()
+    # ws_main_loop()
+    asyncio.run(ws_main_loop())
+
+    # asyncio.get_event_loop().run_until_complete(start_server)
+    # asyncio.get_event_loop().run_forever()
 
 
 
