513b1cbb9aabb6a7f51c9eb035c15d9e71e34187
Kyle Nahas <kylenahas@gmail.com>
2022-05-17 05:55:16 +0000

    Multiple addressed client connections working
    - Address is specified by "http://{ip}?a=1"
    - Webpage can print debug values by appending "&debug" to URL
    - Webpage will try to automatically refresh when it loses connection
    - Python script will dump the DMX queue when it overruns the sACN speed. Care has been taken so this shouldn't be noticeable. TODO: look for better solution

--- main.py
+++ main.py
@@ -13,59 +13,50 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-# ola_recv_dmx.py
-# Copyright (C) 2005 Simon Newton
+# main.py
+# Copyright (C) 2022 Kyle Nahas
 
-"""Receive DMX data."""
+"""Receive DMX data and transmit it to connected clients via websockets."""
 
 import getopt
-import queue
 import textwrap
 import sys
 import threading
-from queue import Queue
 
 from ola.ClientWrapper import ClientWrapper
 
 
 import asyncio
-import datetime
-import random
 import websockets
 import netifaces
 
-# dmx_frame_queue = Queue(maxsize=100)
-dmx_frame_queue = asyncio.Queue(maxsize=10000)
+QUEUE_DEPTH = 100
+# dmx_frame_queue = asyncio.queues.LifoQueue(maxsize=1000) # Moved within scope of event loop
 ws_active_connections = set()
-sys_ip_address = netifaces.ifaddresses('enp0s3')[netifaces.AF_INET][0].get('addr')
+ws_addresses = dict()
+sys_ip_address = netifaces.ifaddresses('enp0s3')[netifaces.AF_INET][0].get('addr') # Get the IP address of the first network card
 
 
 async def serve_data_to_all_clients():
     while True:
-        print("Server")
         frame = await dmx_frame_queue.get()
-        for websocket in ws_active_connections:
-            print("Sending to: " + str(websocket.id))
-
-            print("Data: " + str(frame[0:3]))
-            offset = 0
-            color = 'rgb(' + str(frame[0 + offset]) + ', ' + str(frame[1 + offset]) + ', ' + str(
-                frame[2 + offset]) + ')'
-            await websocket.send(color)
-
-        # await asyncio.sleep(0.1)
-
-
-
-
-async def serve_latest_color(websocket, path):
-    while True:
-        if not dmx_frame_queue.empty():
-            frame = dmx_frame_queue.get()
-            print("Data: " + str(frame[0:3]))
-            offset = 0
-            color = 'rgb(' + str(frame[0+offset]) + ', ' + str(frame[1+offset]) + ', ' + str(frame[2+offset]) + ')'
-            await websocket.send(color)
+        if len(ws_active_connections):
+            await asyncio.sleep(0.1)
+            for websocket in ws_active_connections:
+                # print("Sending to: " + str(websocket.id))
+                # print("Data: " + str(frame[0:3]))
+                offset = ws_addresses[websocket.id] - 1
+                color = 'rgb(' + str(frame[0 + offset]) + ', ' + str(frame[1 + offset]) + ', ' + str(
+                    frame[2 + offset]) + ')'
+                try:
+                    await websocket.send(color)
+                except websockets.ConnectionClosed:
+                    ws_active_connections.remove(websocket)
+                    print("Tried to send to a websocket that no longer exists")
+                    print(" -> Removed: " + str(websocket.id))
+                    break
+        else:
+            await asyncio.sleep(0.5)
 
 
 async def start_server():
@@ -74,48 +65,46 @@ async def start_server():
     async with websockets.serve(ws_handler, sys_ip_address, 5678):
         await asyncio.Future()  # run forever
 
+
 async def ws_handler(websocket):
-    print("New ws client: " + str(websocket.id))
     ws_active_connections.add(websocket)
+    ws_addresses[websocket.id] = 1  # Sane default == 1
+    print("New ws client: " + str(websocket.id))
+    print("There are now " + str(len(ws_active_connections)) + " client(s) connected.")
+    try:
+        message = await websocket.recv()
+        ws_addresses[websocket.id] = int(message)
+        assert(0 < ws_addresses[websocket.id] < 511)
+        print("Client: " + str(websocket.id) + " assigned to address: " + str(ws_addresses.get(websocket.id)))
+    except ValueError:
+        print("Invalid address specified for: " + str(websocket.id) + ". Ignored, address == 1")
+    except AssertionError:
+        print("Out of bounds address specified for: " + str(websocket.id) + ". Ignored, address == 1")
+
     try:
         await websocket.wait_closed()
-    finally:
-        print("Removed ws client: " + str(websocket.id))
         ws_active_connections.remove(websocket)
-
-
-async def send(websocket, message):
-    try:
-        await websocket.send(message)
     except websockets.ConnectionClosed:
-        pass
+        print("Removed ws client: " + str(websocket.id))
+
 
 async def ws_main_loop():
+    global dmx_frame_queue
+    dmx_frame_queue = asyncio.queues.LifoQueue(maxsize=QUEUE_DEPTH)
     await asyncio.gather(
         start_server(),
         serve_data_to_all_clients()
     )
-    # loop = asyncio.new_event_loop()
-    # loop.run_until_complete(start_server())
-    # loop.run_forever()
-    # asyncio.get_event_loop().run_until_complete(start_server())
-    # asyncio.get_event_loop().run_forever()
-    # asyncio.set_event_loop(start_server())
-    # asyncio.get_event_loop().run_forever()
 
 
 def NewData(data):
     try:
         dmx_frame_queue.put_nowait(data)
     except asyncio.queues.QueueFull:
-        print("DMX Queue full")
-    # frame = data
-    # print("Data: " + str(frame[0:3]))
-    # offset = 0
-    # color = 'rgb(' + str(frame[0 + offset]) + ', ' + str(frame[1 + offset]) + ', ' + str(frame[2 + offset]) + ')'
-    # for websocket in ws_active_connections.copy():
-    #     asyncio.create_task(send(websocket, color))
-    # print(data[0:3])
+        while dmx_frame_queue.qsize():
+            dmx_frame_queue.get_nowait()
+        dmx_frame_queue.put_nowait(data)
+        print("DMX Queue Dumped")
 
 
 def Usage():
@@ -127,50 +116,40 @@ def Usage():
   -h, --help                Display this help message and exit.
   -u, --universe <universe> Universe number."""))
 
-async def to_thread(func, /, *args, **kwargs):
-    loop = asyncio.get_running_loop()
-    ctx = contextvars.copy_context()
-    func_call = functools.partial(ctx.run, func, *args, **kwargs)
-    return await loop.run_in_executor(None, func_call)
 
 def ola_main_loop():
-  try:
-      opts, args = getopt.getopt(sys.argv[1:], "hu:", ["help", "universe="])
-  except getopt.GetoptError as err:
-    print(str(err))
-    Usage()
-    sys.exit(2)
+    print("OLA Server Started:")
+    universe = 2
+    print(" -> Subscribed to universe: " + str(universe))
+    # try:
+    #     opts, args = getopt.getopt(sys.argv[1:], "hu:", ["help", "universe="])
+    # except getopt.GetoptError as err:
+    #     print(str(err))
+    #     Usage()
+    #     sys.exit(2)
+    #
+    #
+    # for o, a in opts:
+    #     if o in ("-h", "--help"):
+    #       Usage()
+    #       sys.exit()
+    #     elif o in ("-u", "--universe"):
+    #       universe = int(a)
 
-  universe = 1
-  for o, a in opts:
-    if o in ("-h", "--help"):
-      Usage()
-      sys.exit()
-    elif o in ("-u", "--universe"):
-      universe = int(a)
-
-  wrapper = ClientWrapper()
-  client = wrapper.Client()
-  client.RegisterUniverse(universe, client.REGISTER, NewData)
-  wrapper.Run()
+    wrapper = ClientWrapper()
+    client = wrapper.Client()
+    client.RegisterUniverse(universe, client.REGISTER, NewData)
+    wrapper.Run()
 
 
 if __name__ == "__main__":
-    print("OLA Server Started:")
-    ws_thread = threading.Thread(target=ws_main_loop)
+
     ola_thread = threading.Thread(target=ola_main_loop)
 
-    # ws_thread.start()
     ola_thread.start()
 
-    # to_thread(ola_main_loop())
-    # to_thread(start_server())
-    # ws_main_loop()
-    # ws_main_loop()
     asyncio.run(ws_main_loop())
 
-    # asyncio.get_event_loop().run_until_complete(start_server)
-    # asyncio.get_event_loop().run_forever()
 
 
 
